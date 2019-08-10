3fa409bd8506f1b595a7b7596446312208a04d0a
David Yoon <davidyoon85@gmail.com>
2019-08-10 15:47:35 -0400

    add users, usersitem and spinner

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
+
+import Navbar from "./components/layout/Navbar";
+import Users from "./components/layout/Users";
+import axios from "axios";
+
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
-  );
+class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  };
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const res = await axios.get("https://api.github.com/users");
+    this.setState({ users: res.data, loading: false });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <div className="container">
+          <Users loading={this.state.loading} users={this.state.users} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
