874425f8563551ba46ae2ea7f16d0d228526b591
David Yoon <davidyoon85@gmail.com>
2019-08-13 15:40:01 -0400

    add home & notfound

--- src/components/layout/Search.js
+++ src/components/layout/Search.js
@@ -1,45 +1,53 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import GithubContext from "../../context/github/githubContext";
+import AlertContext from "../../context/alert/alertContext";
 
-const Search = ({ searchUsers, showClear, clearUsers, setAlert }) => {
-  const [text, setText] = useState('');
+const Search = () => {
+  const githubContext = useContext(GithubContext);
+  const alertContext = useContext(AlertContext);
+
+  const [text, setText] = useState("");
 
   const handleChange = e => {
-    setText(e.target.value)
+    setText(e.target.value);
   };
 
   const handleSubmit = e => {
     e.preventDefault();
     if (text === "") {
-      setAlert("Please enter something", "light");
+      alertContext.setAlert("Please enter something", "light");
     } else {
-      searchUsers(text);
-      setText('');
+      githubContext.searchUsers(text);
+      setText("");
     }
   };
 
-    return (
-      <div>
-        <form className="form" onSubmit={handleSubmit}>
-          <input
-            type="text"
-            name="text"
-            value={text}
-            onChange={handleChange}
-            placeholder="Search Users.."
-          />
-          <input
-            type="submit"
-            value="Search"
-            className="btn btn-dark btn-block"
-          />
-        </form>
-        {showClear && (
-          <button className="btn btn-light btn-block" onClick={clearUsers}>
-            Clear
-          </button>
-        )}
-      </div>
-    );
-}
+  return (
+    <div>
+      <form className="form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="text"
+          value={text}
+          onChange={handleChange}
+          placeholder="Search Users.."
+        />
+        <input
+          type="submit"
+          value="Search"
+          className="btn btn-dark btn-block"
+        />
+      </form>
+      {githubContext.users.length > 0 && (
+        <button
+          className="btn btn-light btn-block"
+          onClick={githubContext.clearUsers}
+        >
+          Clear
+        </button>
+      )}
+    </div>
+  );
+};
 
 export default Search;
--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,9 +1,14 @@
-import React, { Fragment, useEffect } from "react";
+import React, { Fragment, useEffect, useContext } from "react";
 import Spinner from "../layout/Spinner";
 import Repos from "../repos/Repos";
 import { Link } from "react-router-dom";
+import GithubContext from "../../context/github/githubContext";
 
-const User = ({ user, repos, loading, getUser, getUserRepos, match }) => {
+const User = ({ match }) => {
+  const githubContext = useContext(GithubContext);
+
+  const { user, loading, getUser, repos, getUserRepos } = githubContext;
+
   useEffect(() => {
     getUser(match.params.login);
     getUserRepos(match.params.login);
